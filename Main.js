df77637e4a9f40ef8845d9222838b90271290dba
Csanad Kormos <csanadsuli19@gmail.com>
2022-09-20 12:56:16 +0200

    Styluslap elkészítés táblázat beszúrása

--- Main.js
+++ Main.js
@@ -6,8 +6,24 @@ function tesztAlert(){
 function BMIszamitas(){
     let magassag = parseFloat(document.getElementById('magassag').value);
     let suly = parseFloat(document.getElementById('suly').value);
+    let bmi = (suly / (magassag / 100) ** 2).toFixed(1);
 
-    document.getElementById('eredmeny').innerHTML = "Az ön BMI értéke: " + (suly / (magassag / 100) ** 2).toFixed(1);
+    document.getElementById('eredmeny').innerHTML += bmi;
+
+    if(bmi <= 18,5){
+
+    }else if(18,5 < bmi && bmi < 25){
+
+    }else if(25 <= bmi && bmi < 30){
+
+    }else if(25 <= bmi && bmi < 30){
+
+    }else if(25 <= bmi && bmi < 30){
+
+    }else{
+
+    }
+    
 }
 
 document.getElementById('szamitas').addEventListener('click', BMIszamitas);
